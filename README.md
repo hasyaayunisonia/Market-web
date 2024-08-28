ceed4fca0021cf4245161a24db4e6dd1aff829dd
hasya <hasya.ayuni.tif20@polban.ac.id>
2024-08-28 22:13:02 +0700

    initial projec

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Market-web
+
+Node version
+v18.19.0
+
+yarn run web
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native'; // Import necessary components
+
+const App = () => {
+  return (
+    <SafeAreaView 
+     style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>React Native Fun!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
